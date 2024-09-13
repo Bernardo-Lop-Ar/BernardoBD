027c2621ee30c29ea2dde6d95446b067d6560868
Bernardo Lopes <berlopar04@gmail.com>
2024-09-13 19:49:45 -0300

    Add files via upload

--- ex3.sql
+++ ex3.sql
@@ -1,36 +1,14 @@
-USE ex8;
+USE ex12;
 
-CREATE TABLE produtos (
-	id_produto INT NOT NULL PRIMARY KEY,
-    nome_produto VARCHAR(150) NOT NULL,
-    desc_produto VARCHAR(255) NOT NULL,
-    valor_produto DECIMAL(10,2) NOT NULL
-    );
-
-CREATE TABLE clientes (
-	id_cliente INT NOT NULL PRIMARY KEY,
-    nome_cliente VARCHAR(100) NOT NULL,
-    email_cliente VARCHAR(100) NOT NULL,
-    telefone_cliente VARCHAR(15) NOT NULL
+CREATE TABLE Pessoas (
+	id_pessoa INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) DEFAULT NULL,
+    telefone VARCHAR(15) DEFAULT NULL
     );
     
-CREATE TABLE pedidos (
-	id_pedido INT NOT NULL PRIMARY KEY,
-    desc_pedido VARCHAR(255) NOT NULL,
-    id_cliente INT NOT NULL,
-    id_produto INT NOT NULL,
-    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
-    FOREIGN KEY (id_produto) REFERENCES produtos(id_produto)
+CREATE TABLE Documento (
+	id_documento INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nr_documento INT NOT NULL,
+    id_pessoa INT,
+    FOREIGN KEY (id_pessoa) REFERENCES Pessoas(id_pessoa)
     );
-    
-CREATE TABLE detalhe_pedido (
-	id_produto INT NOT NULL,
-	id_pedido INT NOT NULL,
-    FOREIGN KEY (id_produto) REFERENCES produtos(id_produto),
-    FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido)
-    );
-    
--- As entidades(pedidos, clientes e pedidos) são primárias, já detalhe_pedido é uma entidade fraca
--- dependendo de outras duas, o relacionamento entre produtos e pedidos é de muitos para muitos
--- já a relação entre Clientes e Pedidos é diferente, pois um cliente pode fazer vários pedidos, 
--- mas cada pedido é feito por um cliente
